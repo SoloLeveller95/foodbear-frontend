83466409a96faf8f17b256a558c3f6b48369b122
Fathullah Harun <sololeveller@gmail.com>
2022-08-18 18:34:15 +0800

    Update delivery screens

--- screens/DeliveryScreen.tsx
+++ screens/DeliveryScreen.tsx
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, View } from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParam } from "../App";
 
 export default function DeliveryScreen() {
+	const navigation = useNavigation<NativeStackNavigationProp<RootStackParam>>();
+
+	useEffect(() => {
+		setTimeout(() => {
+			navigation.navigate("Home");
+		}, 5000);
+	}, []);
+
 	return (
-		<View>
-			<Text>DeliveryScreen</Text>
+		<View
+			style={{
+				flex: 1,
+				alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			<Image
+				style={{ height: "100%", width: "100%", paddingHorizontal: 100 }}
+				source={require("../assets/delivery7.png")}
+			/>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({});
